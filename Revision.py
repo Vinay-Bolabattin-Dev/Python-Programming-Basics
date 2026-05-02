700abb4fc8e4a5c0ca672aa17d415a40fa98d948
Vinay-Bolabattin-Dev <vinaybolabattin@gmail.com>
2026-05-02 12:14:34 +0530

    feat: complete lists and tuples revision.

--- Revision.py
+++ Revision.py
@@ -31,17 +31,56 @@
 
 
 
-""" String 
-practice + Revision  """
+# """ String 
+# practice + Revision  """
 
-movie_name = "2026_PROJECT_CLIP_01.MP4"
+# movie_name = "2026_PROJECT_CLIP_01.MP4"
 
 
-movie_name.strip() # removing extra space 
+# movie_name.strip() # removing extra space 
 
-movie_name[0:5] # printing only year from name 
+# movie_name[0:5] # printing only year from name 
 
-movie_name.replace("CLIP", "SCENE") # replacing clip to scene 
+# movie_name.replace("CLIP", "SCENE") # replacing clip to scene 
 
-final_moviename= movie_name.lower()
-print(final_moviename)
+# final_moviename= movie_name.lower()
+# print(final_moviename)
+
+
+# ## List 
+# assets = ["intro", "vlog_clip", "outro"]
+
+
+# assets[1 ]= "cinematic_shot"
+
+# assets.append("Background_music")
+
+# assets.sort()
+# print(assets)
+
+# ## slicing in list 
+# a= assets[0:2]
+# print(a)
+
+## TUPLES
+#Example 1
+# required_resolution=(1970, 1080)
+# width=int(input('Enter a width: '))
+# height= int (input("Enter a height: "))
+
+# if width == required_resolution[0] and required_resolution[1]:
+#     print("Resolution Match! Ready to export")
+
+# else:
+#     print("Warning: Resolution mismatch.")
+
+#example 2:
+
+peack_hours=(10,14, 20) #(representing 10 AM, 2 PM, and 8 PM).
+
+post_time= int(input("Enter you post time: "))
+
+if post_time == peack_hours[0] or post_time== peack_hours[2]:
+    print("Excellent timing! High engagement expected")
+else:
+    print("standard post time ")
