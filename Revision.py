7de431e2a12dc09c09fedef7b57ceef530e09763
Vinay  Bolabattin <vinaybolabattin@gmail.com>
2026-05-22 12:03:52 +0530

    created new file Advance-python.py for advance topics

--- Revision.py
+++ Revision.py
@@ -1435,41 +1435,5 @@ Access Modifiers:
 # print(video.get())
 
 
-""" final Expection/Error handling challeges"""
-
-# video_duration=30
-# video_duration=0
-
-# try:
-#     render_speed=100/video_duration
-
-# except ZeroDivisionError:
-#     print("Error: Duration can not be 0 secounds ")
-
-# else:
-#     print("Render successful !! Speed score caculated ")
-
-# finally:
-#     print("Closing rendering engine claering temporary cache")
-
-
-""" raise keyword in exception handling """
-
-export_fps = int(input("Enter a export_fps: "))
-
-try:
-    if export_fps > 60:
-        raise ValueError("CapCut only supports up to 60 Fps for Instagram reels!!")
-    else:
-        print("Fps setting verified !!")
-
-except ValueError as e :
-    print(e)
-
-finally :
-    print('Export configuration complete')
-     
-
-
 
 
