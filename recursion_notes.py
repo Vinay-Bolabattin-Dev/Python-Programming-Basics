bf90ed0c331b6e67ddda9c0c78d00ec0da1c96da
Vinay  Bolabattin <vinaybolabattin@gmail.com>
2026-06-02 13:41:38 +0530

    Implement basic recursive math and list tracking functions

--- recursion_notes.py
+++ recursion_notes.py
@@ -1,1 +1,37 @@
+# def find_factorial(n):
+#     # if n ==1:
+#     #     return 1
+    
+    
+#     return n*find_factorial(n-1)
+# result=find_factorial(4)
+# print(f"Factorial is {result}")
 
+## practically example 
+## 1.1
+# def count_up(num):
+#     if num==0:
+#         return 
+#     count_up(num-1)
+#     print(num)
+
+# number = count_up(10)
+
+## 1.2
+# def calculate_sum(n):
+#     if n==1:
+#         return 1
+#     return n+calculate_sum(n-1)
+
+# num= calculate_sum(4)
+# print(num)
+
+## 1.3 
+
+def count_elements(my_list):
+    if my_list==[]:
+        return 0
+    return 1+count_elements(my_list[1:])
+
+iteams = count_elements([10,20,30,40])
+print(iteams)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,58 +1,37 @@
-## Question 1.
-# class StringUtility:
-#     def forrmat_title(self, raw_test):
-#         self.raw_test = raw_test
-#         return raw_test.title()
-        
-# text1=StringUtility()
-# print(text1.forrmat_title("python programming"))
-
-
+# def find_factorial(n):
+#     # if n ==1:
+#     #     return 1
     
-# ##Question 2.
-# class SmartDevice:
-#     def __init__(self,device_name , batter_level):
-#         self.device_name=device_name
-#         self.batter_level=batter_level
-#         print( f'Device {self.device_name} is boosted up' )
     
-# D1=SmartDevice("motorola", 90)
+#     return n*find_factorial(n-1)
+# result=find_factorial(4)
+# print(f"Factorial is {result}")
 
-# del D1
-# print(D1)
+## practically example 
+## 1.1
+# def count_up(num):
+#     if num==0:
+#         return 
+#     count_up(num-1)
+#     print(num)
 
+# number = count_up(10)
 
-##Question 3
+## 1.2
+# def calculate_sum(n):
+#     if n==1:
+#         return 1
+#     return n+calculate_sum(n-1)
 
-# class Notification:
-#     def send(self):
-#         pass
+# num= calculate_sum(4)
+# print(num)
 
+## 1.3 
 
-# class SMSNotification(Notification):
-#     def send(self):
-#         super().send()
-#         print("its hot out side try to stay in home ")
+def count_elements(my_list):
+    if my_list==[]:
+        return 0
+    return 1+count_elements(my_list[1:])
 
-# msg=SMSNotification()
-# msg.send()
-
-##Question 4
-class profile:
-    def set_bio(self, text , links=None):
-        self.text=text
-        self.links=links
-
-        print(f'Bio Text: {self.text}')
-
-        if links is not None:
-            print(f'links provided {self.links}')
-
-
-
-    
-# e1=profile()
-# e1.set_bio("jay shree ram")
-
-e2=profile()
-e2.set_bio("jay shree ram ", ["http://google.com"])
+iteams = count_elements([10,20,30,40])
+print(iteams)
