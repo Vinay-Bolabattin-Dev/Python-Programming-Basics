cc317ae458dd79ecf12757b6a3a67a793d14d3f3
Vinay  Bolabattin <vinaybolabattin@gmail.com>
2026-06-11 20:41:47 +0530

    Implement dictionary core syntax and data retrieval mechanics

---
+++ json_dictionary_notes.py
@@ -0,0 +1,9 @@
+site_crew={
+    "role": 2520,
+    "names": "mahesh"
+}
+print(site_crew)
+print(f'name of worker is : {site_crew["names"]}')
+print(f'role number  of worker is : {site_crew["role"]}')
+site_crew["role"]=2000
+print(site_crew)
