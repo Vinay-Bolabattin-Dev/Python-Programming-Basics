a94088ed580f15d961243b62d1c9e2c74bc7aad3
Vinay  Bolabattin <vinaybolabattin@gmail.com>
2026-06-20 11:44:48 +0530

    Complete Medium Level challenge for dynamic secure API routing simulation

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -17,16 +17,41 @@
 #     print(f"Error: Unknown server issue. Status code: {response.status_code}")
 
 """ Easy level test """
+# class MockResponse:
+#     def __init__(self, status_code, data):
+#         self.status_code=status_code
+#         self.data=data 
+
+# response=MockResponse(404, '{"status": "active", "total_reels":14}')
+
+# if response.status_code==200:
+#     print("print a success message")
+# elif response.status_code==404:
+#     print(" Not found Error message ")
+# else:
+#     print('unkown server issue')
+
+""" Easy level test """
 class MockResponse:
     def __init__(self, status_code, data):
         self.status_code=status_code
         self.data=data 
 
-response=MockResponse(404, '{"status": "active", "total_reels":14}')
+def secure_api_request(url , token ):
+    if url=="http://api.securedata.com" and token == "securet_token_123":
+        return MockResponse(200,'{"access": "granted"}')
+    elif url=="http://api.securedata.com" and token !="securet_token_123":
+        return MockResponse(401,'{"error": "Invalid Token"}')
+    else:
+        return MockResponse(404, "")
+        
+result = secure_api_request("http://api.securedata.com", "securet_token_1234")  
 
-if response.status_code==200:
-    print("print a success message")
-elif response.status_code==404:
-    print(" Not found Error message ")
-else:
-    print('unkown server issue')
+
+if result.status_code == 200:
+    print(f"Success: {result.data}")
+elif result.status_code == 401:
+    print("Access Denied: Bad Credentials")
+elif result.status_code == 404:
+    print("Not Found") 
+
