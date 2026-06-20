3523bd69286359bcea2abfbbca9444ec12ea20e2
Vinay  Bolabattin <vinaybolabattin@gmail.com>
2026-06-20 20:38:02 +0530

    Complete video size API status code simulation logic

--- api_notes.py
+++ api_notes.py
@@ -32,26 +32,52 @@
 #     print('unkown server issue')
 
 """ Easy level test """
+# class MockResponse:
+#     def __init__(self, status_code, data):
+#         self.status_code=status_code
+#         self.data=data 
+
+# def secure_api_request(url , token ):
+#     if url=="http://api.securedata.com" and token == "securet_token_123":
+#         return MockResponse(200,'{"access": "granted"}')
+#     elif url=="http://api.securedata.com" and token !="securet_token_123":
+#         return MockResponse(401,'{"error": "Invalid Token"}')
+#     else:
+#         return MockResponse(404, "")
+        
+# result = secure_api_request("http://api.securedata.com", "securet_token_1234")  
+
+
+# if result.status_code == 200:
+#     print(f"Success: {result.data}")
+# elif result.status_code == 401:
+#     print("Access Denied: Bad Credentials")
+# elif result.status_code == 404:
+#     print("Not Found") 
+
+
+""" Medium level test"""
 class MockResponse:
     def __init__(self, status_code, data):
         self.status_code=status_code
         self.data=data 
 
-def secure_api_request(url , token ):
-    if url=="http://api.securedata.com" and token == "securet_token_123":
-        return MockResponse(200,'{"access": "granted"}')
-    elif url=="http://api.securedata.com" and token !="securet_token_123":
-        return MockResponse(401,'{"error": "Invalid Token"}')
+def upload_video_api(file_size_mb):
+    if file_size_mb <=0:
+        return MockResponse(404,'{"error": "Invalid file size"}')
+    elif file_size_mb>500:
+        return MockResponse(413,'{"error": "file exceeds 500MB limit"}')
     else:
-        return MockResponse(404, "")
+        return MockResponse(200, '{"status": "uploaded", "processing": True }')
         
-result = secure_api_request("http://api.securedata.com", "securet_token_1234")  
 
+upload_result=upload_video_api(600)
 
-if result.status_code == 200:
-    print(f"Success: {result.data}")
-elif result.status_code == 401:
-    print("Access Denied: Bad Credentials")
-elif result.status_code == 404:
-    print("Not Found") 
-
+if upload_result.status_code==200:
+    print(f"Success: {upload_result.data}")
+elif upload_result.status_code==400:
+    print(f"Error :{upload_result.data}")
+elif upload_result.status_code==413:
+    print(f"playload {upload_result.data}")
+else:
+    print("Not Found ")
